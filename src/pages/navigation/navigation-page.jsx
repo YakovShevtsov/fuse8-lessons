d5ef7d7b368c2563da747dd1c772058d0da44645
Yakov <yakov.shev@gmail.com>
2025-03-21 15:17:33 +0100

    [Lesson4] Generating navigation list with permissions; Rendering it dynamically

--- src/pages/navigation/navigation-page.jsx
+++ src/pages/navigation/navigation-page.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import './navigation-page.scss';
 
 const USER_READ_PERMISSIONS = [
@@ -46,7 +47,7 @@ const routes = {
     name: 'partners',
     pathname: 'partners',
     getLink: () => '/partners',
-    text: 'Клиенты',
+    text: 'Партнеры',
   },
 };
 
@@ -80,12 +81,29 @@ const navigationList = [
   },
 ];
 
-// Нужно написать функцию
 const generateNavigationListWithPermissions = (
   navigationList,
   checkPermission
 ) => {
-  return [];
+  return navigationList
+    .map((level1) => {
+      const firstLevelChilds = level1.children
+        .map((level2) => {
+          const secondLevelChilds = level2.children.filter((route) =>
+            checkPermission(route.name)
+          );
+
+          return secondLevelChilds.length > 0
+            ? { ...level2, children: secondLevelChilds }
+            : null;
+        })
+        .filter((item) => item !== null);
+
+      return firstLevelChilds.length > 0
+        ? { ...level1, children: firstLevelChilds }
+        : null;
+    })
+    .filter((item) => item !== null);
 };
 
 export const NavigationPage = () => {
@@ -96,36 +114,31 @@ export const NavigationPage = () => {
 
   return (
     <div className="container">
-      {/* {navigationListWithPermission.map((item) => item)} */}
-
-      <div className="navigation">
-        <div className="navigation-level-1">
-          Контент
-          <div className="navigation-level-2">
-            Работа
-            <div className="navigation-level-3">
-              {checkHasUserPermission('vacancies') && <div>Вакансии</div>}
-              {checkHasUserPermission('candidates') && <div>Кандидаты</div>}
-            </div>
-          </div>
-          <div className="navigation-level-2">
-            Новости
-            <div className="navigation-level-3">
-              {checkHasUserPermission('events') && <div>События</div>}
-            </div>
-          </div>
-        </div>
-        <div className="navigation-level-1">
-          Пользователи
-          <div className="navigation-level-2">
-            Клиенты
-            <div className="navigation-level-3">
-              {checkHasUserPermission('clients') && <div>Клиенты</div>}
-              {checkHasUserPermission('partners') && <div>Партнеры</div>}
-            </div>
-          </div>
-        </div>
-      </div>
+      <nav>
+        <ul className="navigation">
+          {navigationListWithPermission.map((level1) => (
+            <li className="navigation-level-1" key={level1.text}>
+              {level1.text}
+              <ul className="navigation-submenu">
+                {level1.children.map((level2) => (
+                  <li className="navigation-level-2" key={level2.text}>
+                    {level2.text}
+                    <ul className="navigation-level-3">
+                      {level2.children.map((route) => (
+                        <li>
+                          <Link to={route.getLink()} key={route.text}>
+                            {route.text}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   );
 };
